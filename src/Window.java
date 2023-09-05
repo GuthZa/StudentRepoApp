c951ccf6931732b4714570a4f3263bd6041ad1ff
GuthZa <joao9.claro99@gmail.com>
2023-09-05 13:14:30 +0100

    show students in a table

--- src/Window.java
+++ src/Window.java
@@ -4,36 +4,22 @@ import java.awt.*;
 public class Window extends JFrame {
 
     public Window() throws HeadlessException {
-        JTextField nameTextField = new JTextField();
-        nameTextField.setBounds(10, 10, 200, 25);
-
-        JButton button = new JButton("click");
-        button.setBounds(70, 70, 100, 50);
-
-        JLabel nameLabel = new JLabel("placeholder");
-        nameLabel.setBounds(10, 150, 100, 25);
-
         String[][] data = {
                 {"1", "Bilbo Baggins", "Shire"},
                 {"2", "Samwise Gamgee", "Shire"},
                 {"3", "Gandalf", "Wanderer"}};
-        String[] columns = {"ID", "Name", "Location"};
-        JTable table = new JTable(data, columns);
+        String[] column = {"ID", "NAME", "LOCATION"};
 
-        table.setBounds(350, 10, 250, 250);
-        table.setCellSelectionEnabled(false);
-        table.setDefaultEditor(Object.class, null);
-        table.sizeColumnsToFit(1);
+        final JTable jt=new JTable(data,column);
+        jt.setCellSelectionEnabled(false);
+        jt.setDefaultEditor(Object.class, null);
+        JScrollPane sp=new JScrollPane(jt);
 
-        add(button);
-        add(nameTextField);
-        add(nameLabel);
-        add(table);
+        add(sp);
 
 
         setSize(700, 700);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setLayout(null);
         setVisible(true);
         setResizable(false);
     }
