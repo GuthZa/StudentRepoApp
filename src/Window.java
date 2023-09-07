db67d529b7f94004998d920d387faa68bdaa2908
GuthZa <joao9.claro99@gmail.com>
2023-09-07 10:24:38 +0100

    added some buttons and text entry fields

--- src/Window.java
+++ src/Window.java
@@ -12,7 +12,7 @@ public class Window extends JFrame {
     public Window() throws HeadlessException {
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setBounds(100, 100, 970, 920);
+        setBounds(100, 100, 670, 620);
         setTitle("Student Management Window");
 
         Container contentPane = getContentPane();
@@ -22,33 +22,82 @@ public class Window extends JFrame {
                 {"1", "Bilbo", "Baggins", "Shire", "10"},
                 {"2", "Samwise", "Gamgee", "Shire", "10"},
                 {"3", "Gandalf", "the Grey", "Wanderer", "20"}};
-        String[] column = {"ID", "NAME", "LOCATION"};
 
+        //Text Fields
+
+        //First Name
+        JLabel firstNameLabel = new JLabel("First name: ");
+        firstNameLabel.setBounds(15 , 35, 150, 34);
+        contentPane.add(firstNameLabel);
+
+        JTextField firstNameTextField = new JTextField();
+        firstNameTextField.setBounds(95, 40, 200, 30);
+        contentPane.add(firstNameTextField);
+
+        //Last Name
+        JLabel lastNameLabel = new JLabel("Last name: ");
+        lastNameLabel.setBounds(15 , 85, 150, 34);
+        contentPane.add(lastNameLabel);
+
+        JTextField lastNameTextField = new JTextField();
+        lastNameTextField.setBounds(95, 90, 200, 30);
+        contentPane.add(lastNameTextField);
+
+        //Location
+        JLabel locationLabel = new JLabel("Location: ");
+        locationLabel.setBounds(15 , 135, 150, 34);
+        contentPane.add(locationLabel);
+
+        JTextField locationTextField = new JTextField();
+        locationTextField.setBounds(95, 140, 200, 30);
+        contentPane.add(locationTextField);
+
+        //Grade
+        JLabel gradeLabel = new JLabel("Grade: ");
+        gradeLabel.setBounds(20 , 185, 150, 34);
+        contentPane.add(gradeLabel);
+
+        JTextField gradeTextField = new JTextField();
+        gradeTextField.setBounds(95, 190, 200, 30);
+        contentPane.add(gradeTextField);
+
+
+        //Buttons
         JButton addButton = new JButton("Add");
-        addButton.setBounds(400, 120, 120, 40);
+        addButton.setBounds(400, 35, 100, 40);
         contentPane.add(addButton);
 
+        JButton searchButton = new JButton("Search");
+        searchButton.setBounds(400, 95, 100, 40);
+        contentPane.add(searchButton);
 
+        JButton deleteButton = new JButton("Delete");
+        deleteButton.setBounds(400, 175, 100, 40);
+        contentPane.add(deleteButton);
+
+
+
+        //Table
         JTable table = new JTable();
         DefaultTableModel model = new DefaultTableModel();
         model.setColumnIdentifiers(columns);
         table.setDefaultEditor(Objects.class, null);
         table.setRowHeight(35);
         table.setBackground(Color.white);
-        table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
 
+        //add row data to table
         for (String[] datum: data) {
             model.addRow(datum);
         }
 
+        //Setting the table to the scrollPane and into the window
         table.setModel(model);
-
         JScrollPane scrollPane = new JScrollPane(table);
-        scrollPane.setBounds(10, 540, 925, 290);
+        scrollPane.setBounds(10, 280, 635, 290);
         contentPane.add(scrollPane);
 
 
-//        setSize(700, 700);
         setVisible(true);
         setResizable(false);
     }
